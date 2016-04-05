15745cc896ce7912fc63d4fe566fbdbb4f0b93b7
Jens Nordgaard <jenordgaard@gmail.com>
2016-04-04 20:40:07 -0700

    First Commit

---
+++ store_items.rb
@@ -0,0 +1,20 @@
+tee_shirts = [
+  {
+price: 25
+size: "Large"
+color: "Purple"
+},
+
+  {
+:price => 30
+:size => "Small"
+:color => "Green"
+},
+
+  {
+price: 40
+size: "X-Large"
+color: "red"
+},
+
+]
