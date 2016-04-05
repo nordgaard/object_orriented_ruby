f463789bb1a775ab75c78a8a5e9de21b53af76b0
Jens Nordgaard <jenordgaard@gmail.com>
2016-04-04 20:52:44 -0700

    indentation adjustment

--- store_items.rb
+++ store_items.rb
@@ -1,26 +1,26 @@
 tee_shirts = [
   {
-price: 25
-size: "Large"
-color: "Purple"
-},
+    price: 25
+    size: "Large"
+    color: "Purple"
+    },
 
-  {
-:price => 30
-:size => "Small"
-:color => "Green"
-},
+    {
+    :price => 30
+    :size => "Small"
+    :color => "Green"
+    },
 
-  {
-price: 40
-size: "X-Large"
-color: "red"
-},
+    {
+    price: 40
+    size: "X-Large"
+    color: "red"
+    },
 
-  {
-price: 50
-size: "chubby"
-color: "pink"
-},
+    {
+    price: 50
+    size: "chubby"
+    color: "pink"
+    },
 
-]
+    ]
